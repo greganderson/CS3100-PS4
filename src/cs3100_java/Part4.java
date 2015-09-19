db97ce3d65637b116086c3ac823b49f43f28c108
Greg Anderson <greg@infobytes.com>
2015-09-19 14:15:21 -0600

    Finished the java version of part 4.

--- src/cs3100_java/Part4.java
+++ src/cs3100_java/Part4.java
@@ -1,7 +1,29 @@
 package cs3100_java;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by greg on 9/17/15.
  */
 public class Part4 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Pattern p = Pattern.compile("(\\\"(\\\\.)*.*?(\\\\.)*(?<!\\\\)\\\")");
+
+        String input = "";
+
+        while (s.hasNextLine()) {
+            input += s.nextLine() + "\n";
+        }
+
+        Matcher m = p.matcher(input);
+        String result = "";
+        while (m.find()) {
+            result += m.group() + "\n";
+        }
+        System.out.println(result);
+    }
 }
--- src/cs3100_java/Part5.java
+++ src/cs3100_java/Part5.java
@@ -1,7 +1,24 @@
 package cs3100_java;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by greg on 9/17/15.
  */
 public class Part5 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Pattern p = Pattern.compile("");
+
+        String input = "";
+
+        while (s.hasNextLine()) {
+            input += s.nextLine();
+        }
+
+        Matcher m = p.matcher(input);
+    }
 }
